8bd027e4f0afa0f5830f5fdb2e1b1f7fe6ed372c
Sergio Izquierdo Clase <seizquie@gmail.com>
2023-12-18 16:12:54 +0100

    Añadir segundo ejercicio

--- Boletin2/Ej01/ej1.php
+++ Boletin2/Ej01/ej1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario sencillo con GET</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,30 +15,34 @@
     
 </head>
 <body>
-    <div class="h1">
-        <h1><span class="h1">Formulario con GET</span></h1>   
-    </div>
-    <?php
-    // Verificar si el usuario ha enviado el formulario, si es así le muestro lo que ha enviado con una bienvenida
-        if(isset($_GET['enviar'])) {
-            $nombre = $_GET['nombre'];
-            $correo = $_GET['correo'];
-    ?>
-    <p class ="p-resultado">El nombre introducido ha sido: <?php print $nombre ?> y el correo ha sido <?php print $correo ?> </p>            
-    <?php
-    // Si no lo ha enviado, se lo muestro para que lo rellene y envíe
-        }else{
-    ?>
-    <div class="container">
-        <!-- El action es para que se ejecute el código del mismo PHP en vez de enviarlo a otro PHP -->
-        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="get">
-        <label class="nombre-label" for="nombre">Nombre</label></br>
-                    <input class="nombre-input" type="text" name="nombre" placeholder="Nombre" /></br>
-                    <label class="correo-input" for="correo">Correo electrónico</label></br>    
-                    <input class="correo-input" type="text" name="correo" placeholder="Correo" /></br>
-                    <button type="submit" class="submit btn btn-info" name="enviar">Enviar</button>
-                </form>
+    <div class="content">
+        <div class="container">
+            <div class="h1">
+                <h1>Formulario con GET</h1>   
             </div>
+        <?php
+        // Verificar si el usuario ha enviado el formulario, si es así le muestro lo que ha enviado con una bienvenida
+            if(isset($_GET['enviar'])) {
+                $nombre = $_GET['nombre'];
+                $correo = $_GET['correo'];
+        ?>
+        <div class="resultado">
+            <p>El nombre introducido ha sido: <?php print $nombre ?> y el correo ha sido <?php print $correo ?> </p>            
+        </div>
+        <?php
+        // Si no lo ha enviado, se lo muestro para que lo rellene y envíe
+            }else{
+        ?>
+        <!-- El action es para que se ejecute el código del mismo PHP en vez de enviarlo a otro PHP -->
+            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="get">
+                <label class="nombre-label" for="nombre">Nombre</label>   
+                <input class="nombre-input" type="text" name="nombre" placeholder="Nombre" />
+                <label class="correo-input" for="correo">Correo electrónico</label>
+                <input class="correo-input" type="email" name="correo" placeholder="Correo" />
+                <button type="submit" class="submit btn btn-info" name="enviar">Enviar</button>
+            </form>
+        </div>
+    </div>
     <?php
         }
     ?>
